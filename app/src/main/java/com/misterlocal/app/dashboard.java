abfdca446d704036de3d641e9c1a1f680f584364
tank550 <tankstark0@gmail.com>
2023-11-22 17:02:12 +0100

    Initial commit

--- app/src/main/java/com/misterlocal/app/dashboard.java
+++ app/src/main/java/com/misterlocal/app/dashboard.java
@@ -1,14 +1,125 @@
 package com.misterlocal.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class dashboard extends AppCompatActivity {
 
+    private SearchView searchView;
+
+
+
+    RequestQueue requestQueue;
+     String api="https://misterlocal.000webhostapp.com/listcat.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        //recher();
+
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, api, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    int jsonArraySize = jsonArray.length();
+                    String descript, img;
+                    JSONObject jsonObject;
+                    scrollH[] scrollHS = new scrollH[jsonArraySize];
+
+                    for (int i = 0; i < jsonArraySize; i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+
+                        descript = jsonObject.getString("design");
+                        img = jsonObject.getString("img");
+                        Toast.makeText(dashboard.this, img.toString(), Toast.LENGTH_LONG).show();
+
+                        scrollHS[i] = new scrollH(img, descript);
+                        fragmentTransaction.add(R.id.scrollh, scrollHS[i]);
+                    }
+                    fragmentTransaction.commit();
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("ERREURSCROLL", "onResponse"+error.getMessage());
+                Toast.makeText(dashboard.this, "Erreur verifier la requete", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue = Volley.newRequestQueue(dashboard.this);
+        requestQueue.add(stringRequest);
     }
-}
+    }
+
+
+    /*public  void recher(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, api, null, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    int jsonArraySize = jsonArray.length();
+                    String descript, img;
+                    JSONObject jsonObject;
+                    scrollH[] scrollHS = new scrollH[jsonArraySize];
+
+                    for (int i = 0; i < jsonArraySize; i++) {
+
+                        jsonObject = jsonArray.getJSONObject(i);
+
+                        descript = jsonObject.getString("design");
+                        img = jsonObject.getString("img");
+                        Toast.makeText(dashboard.this, img.toString(), Toast.LENGTH_LONG).show();
+
+                        scrollHS[i] = new scrollH(img, descript);
+                        fragmentTransaction.add(R.id.scrollh, scrollHS[i]);
+                    }
+                    fragmentTransaction.commit();
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("ERREURSCROLL", "onResponse"+error.getMessage());
+                Toast.makeText(dashboard.this, "Erreur verifier la requete", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue = Volley.newRequestQueue(dashboard.this);
+        requestQueue.add(stringRequest);
+    }
+}*/
--- app/src/main/java/com/misterlocal/app/scrollH.java
+++ app/src/main/java/com/misterlocal/app/scrollH.java
@@ -1,14 +1,48 @@
 package com.misterlocal.app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class scrollH extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
 
+public class scrollH extends Fragment {
+
+    private ImageView imageView;
+    private TextView  textView;
+    private LinearLayout linearLayout;
+    String image, descript;
+    public scrollH(String image, String descript){
+        this.image=image;
+        this.descript=descript;
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scroll_h);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.activity_scroll_h, container, false);
+        this.textView = view.findViewById(R.id.textscrollh);
+        imageView = view.findViewById(R.id.imgscroolh);
+        linearLayout=view.findViewById(R.id.scroolimg);
+        textView.setText(descript.toString());
+        String uri="https://misterlocal.000webhostapp.com/"+image;
+        Picasso.get().load(uri).into(imageView);
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(requireActivity(), composantscroolh.class));
+            }
+        });
+        return view;
     }
 }
